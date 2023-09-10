5007275bb6c87867e7cd1fb755eef20342467e74
zzz sss <onlineaccounts998@gmail.com>
2023-09-10 19:00:52 -0400

    auto commit

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -31,3 +31,39 @@
 		}}>Get It</button
 	>
 </div>
+
+<hr />
+
+<div class="flex flex-col min-w-max gap-2 p-2 text-white">
+	<button
+		class="bg-red-400 w-full p-5"
+		on:click={() => {
+			fetch('https://anything-storage-glovbogi2a-uc.a.run.app/auth/login', {
+				method: 'POST',
+				headers: {
+					accept: 'application/json',
+					['content-type']: 'application/json'
+				},
+				body: JSON.stringify({
+					username: 'bbb@bbb.com',
+					password: 'bbbbbb',
+					loc: 'ui'
+				}),
+				credentials: 'include'
+			});
+		}}>LOG IN</button
+	>
+	<button
+		class="w-full bg-blue-400 p-5"
+		on:click={() => {
+			console.log('getit');
+			fetch('https://anything-storage-glovbogi2a-uc.a.run.app/app', {
+				method: 'GET',
+				credentials: 'include'
+			})
+				.then((r) => r.text().then(console.log))
+				.catch((e) => console.log('errorrrrr', e))
+				.then(() => console.log('called /'));
+		}}>Get It</button
+	>
+</div>
