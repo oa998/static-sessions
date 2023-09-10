e563981005767aa054677699926d47d095e4f46f
zzz sss <onlineaccounts998@gmail.com>
2023-09-10 19:17:52 -0400

    auto commit

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,39 +1,44 @@
-<div class="flex flex-col min-w-max gap-2 p-2 text-white">
-	<button
-		class="bg-red-400 w-full p-5"
-		on:click={() => {
-			fetch('https://nginx-anything-storage-glovbogi2a-uc.a.run.app/auth/login', {
-				method: 'POST',
-				headers: {
-					accept: 'application/json',
-					['content-type']: 'application/json'
-				},
-				body: JSON.stringify({
-					username: 'bbb@bbb.com',
-					password: 'bbbbbb',
-					loc: 'ui'
-				}),
-				credentials: 'include'
-			});
-		}}>LOG IN</button
-	>
-	<button
-		class="w-full bg-blue-400 p-5"
-		on:click={() => {
-			console.log('getit');
-			fetch('https://nginx-anything-storage-glovbogi2a-uc.a.run.app/app', {
-				method: 'GET',
-				credentials: 'include'
-			})
-				.then((r) => r.text().then(console.log))
-				.catch((e) => console.log('errorrrrr', e))
-				.then(() => console.log('called /'));
-		}}>Get It</button
-	>
-</div>
+<button
+	class="bg-red-400 w-full p-5"
+	on:click={() => {
+		fetch('https://nginx-anything-storage-glovbogi2a-uc.a.run.app/auth/login', {
+			method: 'POST',
+			headers: {
+				accept: 'application/json',
+				['content-type']: 'application/json'
+			},
+			body: JSON.stringify({
+				username: 'bbb@bbb.com',
+				password: 'bbbbbb',
+				loc: 'ui'
+			}),
+			credentials: 'include'
+		});
+	}}>LOG IN</button
+>
+
+<form method="POST" action="https://nginx-anything-storage-glovbogi2a-uc.a.run.app/auth/login">
+	<input type="text" id="username" name="username" hidden value="bbb@bbb.com" />
+	<input type="text" id="password" name="password" hidden value="bbbbbb" />
+	<button type="submit">Sign In</button>
+</form>
+
+<button
+	class="w-full bg-blue-400 p-5"
+	on:click={() => {
+		console.log('getit');
+		fetch('https://nginx-anything-storage-glovbogi2a-uc.a.run.app/app', {
+			method: 'GET',
+			credentials: 'include'
+		})
+			.then((r) => r.text().then(console.log))
+			.catch((e) => console.log('errorrrrr', e))
+			.then(() => console.log('called /'));
+	}}>Get It</button
+>
 
 <hr />
-
+<!-- 
 <div class="flex flex-col min-w-max gap-2 p-2 text-white">
 	<button
 		class="bg-red-400 w-full p-5"
@@ -66,4 +71,4 @@
 				.then(() => console.log('called /'));
 		}}>Get It</button
 	>
-</div>
+</div> -->
